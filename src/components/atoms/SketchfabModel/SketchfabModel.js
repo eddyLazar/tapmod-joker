240dc33285e1dd501f2df49defcbe74ddfda5c4f
Eddy <eddy.lazar@gmail.com>
2019-07-03 20:01:39 +0300

    SketchfabModel component

---
+++ src/components/atoms/SketchfabModel/SketchfabModel.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useRef, useState } from 'react';
+import SketchfabWrapper from 'sketchfab-api-wrapper';
+
+const options = {
+  camera: 0,
+  autostart: 1,
+  autospin: 0,
+  preload: 1,
+  annotations_visible: 0,
+  animation_autoplay: 1,
+  ui_infos: 0,
+  ui_annotations: 0,
+  ui_controls: 0,
+  ui_stop: 0,
+  ui_help: 0,
+  ui_hint: 0,
+  ui_watermark: 0,
+  transparent: 0,
+  scrollwheel: 1,
+  double_click: 1
+};
+
+export default ({
+  uid = '',
+  onMaterialClick,
+  children = () => {},
+  width = '100%',
+  height = 500
+}) => {
+  const [isLoading, setIsLoaded] = useState(true);
+
+  const sketchFabApiRef = useRef();
+
+  useEffect(() => {
+    const init = async () => {
+      const loadWrapperPromise = new SketchfabWrapper({
+        iframeQuery: `#${uid}`,
+        uid,
+        options,
+        useSingleton: false
+      });
+      const wrapper = await loadWrapperPromise.init();
+
+      const api = { ...wrapper, ...wrapper.api };
+
+      api.addEventListener('click', function(event) {
+        if (onMaterialClick && event.material) {
+          onMaterialClick(event.material, api);
+        }
+      });
+
+      sketchFabApiRef.current = api;
+
+      window.api = api;
+
+      setIsLoaded(false);
+    };
+    init();
+    console.log('from use effect');
+  }, [onMaterialClick, uid]);
+
+  return (
+    <div style={{ position: 'relative', width, height }}>
+      {children(isLoading)}
+      <iframe
+        title={uid}
+        id={uid}
+        height="100%"
+        width="100%"
+        style={{ opacity: isLoading ? 0 : 1, border: 'none' }}
+      />
+    </div>
+  );
+};
---
+++ src/components/atoms/SketchfabModel/SketchfabModel.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SketchfabModel from './SketchfabModel';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+const uid = 'f6a17ff915374ac0a068370ea2fbde89';
+
+storiesOf('atoms|SketchfabModel', module).add('basic', () => (
+  <SketchfabModel uid={uid} onMaterialClick={action('click on material')}>
+    {isLoading => (isLoading ? <h1>loading...</h1> : null)}
+  </SketchfabModel>
+));
