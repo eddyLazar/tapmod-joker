27e38a58d38e3c39f6411d5ab23e35e58e4d12c7
Eddy <eddy.lazar@gmail.com>
2019-07-07 17:01:03 +0300

    diamond responsive

--- src/components/molecules/JackpotDiamond/JackpotDiamond.js
+++ src/components/molecules/JackpotDiamond/JackpotDiamond.js
@@ -4,11 +4,9 @@ import styled from 'styled-components';
 const JackpotDiamondIcon = styled(Icon)`
   position: absolute;
   left: 22px;
-  font-size: 0;
   line-height: 0;
   letter-spacing: -4px;
   color: rgba(240, 146, 53, 0.31);
-  font-size: 90px;
   z-index: 0;
   height: 100%;
   top: 0px;
@@ -17,7 +15,8 @@ const JackpotDiamondIcon = styled(Icon)`
 `;
 
 JackpotDiamondIcon.defaultProps = {
-  type: 'diamond'
+  type: 'diamond',
+  fontSize: [50, 90]
 };
 
 export default JackpotDiamondIcon;
